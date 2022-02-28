9829e4f6aedeeb21d1f4070ea618deebaedbfeff
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-28 16:52:34 +0100

    keep adding structure while watching the lesson

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\post;
+use Faker\Generator as Faker;
+
+$factory->define(post::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
